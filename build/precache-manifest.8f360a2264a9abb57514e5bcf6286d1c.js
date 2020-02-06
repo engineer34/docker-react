4a1fffa6f5067bf0645b7782fb123eb3dd4c8100
tugbakorkmaz <korkmaztugba1989@gmail.com>
2020-02-06 11:08:38 +0300

    initial commit

---
+++ build/precache-manifest.8f360a2264a9abb57514e5bcf6286d1c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deb3b72141be33b2bb2daa85e20a7032",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8559f193e02a8afa4768",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9ac9543f6c87926fbba4",
+    "url": "/static/js/2.f72aa062.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.f72aa062.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8559f193e02a8afa4768",
+    "url": "/static/js/main.76f21074.chunk.js"
+  },
+  {
+    "revision": "79f9183f7146d0318562",
+    "url": "/static/js/runtime-main.fb4de59d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
